ffc3b860638271716346fa54593c5a8d50f74fd4
Siddharth <siddharth.23.2k5@gmail.com>
2026-02-13 12:50:53 +0530

    find union, intersection and difference

---
+++ Practice/32.py
@@ -0,0 +1,8 @@
+s1 = {1, 2, 3, 4}
+s2 = {2, 3, 4, 5}
+union = s1 | s2
+intersection = s1 & s2
+difference = s1 - s2
+print(f"Union: {union}")
+print(f"Intersection: {intersection}")
+print(f"Difference: {difference}")
