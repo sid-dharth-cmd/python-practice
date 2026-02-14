9e39700c842d8d57fb8ed1698daebfabcaa3fe43
Siddharth <siddharth.23.2k5@gmail.com>
2026-02-14 22:49:06 +0530

    updated

--- Practice/45.py
+++ Practice/45.py
@@ -1,9 +1,10 @@
 def checkPrime(n):
+    if n < 2:
+        return False
     for i in range(2, int(n**0.5)+1):
         if n % i == 0:
             return False
-        else:
-            return True
+    return True
 
 n = int(input("Enter a number: "))
 if checkPrime(n):
