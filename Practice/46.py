5f02d36fff4ffe6f92e4995e5d1eb0604a2a6378
Siddharth <siddharth.23.2k5@gmail.com>
2026-02-15 23:02:00 +0530

    Function to return factorial

--- Practice/46.py
+++ Practice/46.py
@@ -1,1 +1,9 @@
-                
+def findFact(n):
+    fact = 1
+    while n > 0:
+        fact *= n
+        n -= 1
+    return fact
+
+n = int(input("Enter a number: "))
+print(f"Factorial of {n} is {findFact(n)}")
